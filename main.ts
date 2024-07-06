689752cab8d5a9fadeb4cb92f660e1fe95fca99c
Asifa Muhammad Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-07-06 10:40:13 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+
+import inquirer from "inquirer";
+
+let user = await inquirer.prompt 
+(
+    [
+        {
+            name: "name",
+            type: 'input',
+            message:"Please enter your name"
+        },
+        {
+            name: "age",
+            type: 'number',
+            message: "Enter your age",
+            
+        },
+        {
+            name: "Hobby",
+            type: "list",
+            message: "Enter your hobby?",
+            choices: ["Coding", "Photography", "Playing"],
+            default: "Nothing",
+        },
+    
+        {
+            name: "Email",
+            type: "checkbox",
+            message: "Enter your valid email",
+            choices: ["asfa123@gmail.com", "asfa134@gmail.com", "asifaqasim2334@gmail.com"],
+            default: "Nothing"
+        }, 
+
+    ]
+)
+console.log(user)
